3570013ad0457562a3fdca98bd7f3adfcadc5946
Duncan Dickinson <dedickinson@users.noreply.github.com>
2026-04-24 13:41:38 +1000

    make fixes

--- packages/kmdb_tooling/test/empty_test.dart
+++ packages/kmdb_tooling/test/empty_test.dart
@@ -12,4 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-void main() {}
+import 'package:test/test.dart';
+
+void main() {
+  test('empty', () => expect(true, isTrue));
+}
