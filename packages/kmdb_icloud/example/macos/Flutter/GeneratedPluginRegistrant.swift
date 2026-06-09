ebd8115e2d47f5105034f03f9bfcbae5c16d243e
Duncan Dickinson <dedickinson@users.noreply.github.com>
2026-06-09 14:43:50 +1000

    Flutter build out

--- packages/kmdb_icloud/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/kmdb_icloud/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import kmdb_icloud
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  KmdbIcloudPlugin.register(with: registry.registrar(forPlugin: "KmdbIcloudPlugin"))
 }
